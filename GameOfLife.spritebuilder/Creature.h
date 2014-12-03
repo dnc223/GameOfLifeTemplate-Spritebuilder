df3812d0b6bcdd9c753c2f57194efc00b6ee6b60
Dan <dnc223@gmail.com>
2014-12-03 12:51:22 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Creature.h
+++ GameOfLife.spritebuilder/Creature.h
@@ -1,11 +1,3 @@
-//
-//  Creature.h
-//  GameOfLife
-//
-//  Created by Dan Chung on 12/3/14.
-//  Copyright (c) 2014 Apportable. All rights reserved.
-//
-
 #import "CCSprite.h"
 
 @interface Creature : CCSprite
