a64f607221a995f7923fc1f5dd8a1e7bf0b7af21
Dan <dnc223@gmail.com>
2014-12-03 11:37:36 -0500

    Automatically Committed for MakeGamesWithUs

--- GameOfLife.spritebuilder/Grid.m
+++ GameOfLife.spritebuilder/Grid.m
@@ -53,4 +53,18 @@ static const int GRID_COLUMNS = 10;
         y += _cellHeight;
     }
 }
+
+- (void)touchBegan:(UITouch *)touch withEvent:(UIEvent *)event
+{
+    CGPoint touchLocation = [touch locationInNode:self];
+    Creature *creature = [self creatureForTouchPosition:touchLocation];
+    creature.isAlive = !creature.isAlive;
+}
+
+- (Creature *)creatureForTouchPosition:(CGPoint)touchPosition
+{
+    int row = touchPosition.y / _cellHeight;
+    int column = touchPosition.x / _cellWidth;
+    return _gridArray[row][column];
+}
 @end
